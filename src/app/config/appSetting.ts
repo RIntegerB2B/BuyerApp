57ad67fc968a3b2f3b34e50f035d071c50eef59d
Chandran <Preveen_Chandran@Dell.com>
2018-08-16 15:28:09 +0530

    Icons

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -2,5 +2,5 @@ export const AppSetting: AppSettingType = {
     awsServiceUrl: 'http://ec2-13-127-165-9.ap-south-1.compute.amazonaws.com:3010/',
     local3010BuyerServiceUrl: 'http://localhost:3002/',
     // serviceUrl: 'http://localhost:3002/'
-    serviceUrl: 'http://13.232.45:3002/'
+    serviceUrl: 'http://13.232.45.85:3002/'
  };
