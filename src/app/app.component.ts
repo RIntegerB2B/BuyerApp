90e5fc870b8e50bd609d44705f252a33338de138
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-12-31 22:54:31 +0530

    changes in video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  routerData: any;
   title = 'app';
+  constructor(private route: Router) {
+    this.routerData = this.route.config;
+    this.route.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    });
+  }
 }
